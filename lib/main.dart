5d637325de09a66623e4e1f36521b63f2603b312
artimmy <arthurtimmy@hotmail.com>
2022-02-07 01:15:00 -0300

    adding main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:heads_tail/play.dart';
 
+void main() => runApp(const MaterialApp(
+  home: Play(),
+  debugShowCheckedModeBanner: false,
+));
